53ad8e73f33b72704641770526a339bd70c1bd08
isatvikmishra <asksatvik@gmail.com>
2024-09-22 11:47:07 +0530

    optimal app added

--- Array Questions/IMP_Second_Smallest_Second_Largest_Element.cpp
+++ Array Questions/IMP_Second_Smallest_Second_Largest_Element.cpp
@@ -1,9 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int main(){
-        int n = 5;
-        vector<int> arr = {1,2,3,4,5};
+   
+int n = 5;
+vector<int> arr = {4,3,4,5,2};
+   
+    /*
         int small = INT_MAX, secondSmall = INT_MAX;
         int large = INT_MIN, secondLarge = INT_MIN;
 
@@ -23,4 +27,30 @@ int main(){
 
     cout << "Second smallest " << secondSmall << endl;
     cout << "Second largest " << secondLarge << endl;
+    */
+
+//!Optimal approach
+ int largest = arr[0];
+ int sLargest = INT_MIN;
+ 
+ int smallest = arr[n-1];
+ int sSmallest = INT_MAX;
+
+ for(int i = 1; i < n; i++){
+    if(arr[i] > largest){
+        largest = arr[i];
+        sLargest = arr[i-1];
+    }
+    else if (arr[i] < smallest){
+        sSmallest = smallest;
+        smallest = arr[i];
+    }
+    else if (arr[i] != smallest && arr[i] < sSmallest){
+        sSmallest = arr[i];
+    }
+ }
+    cout << sLargest << endl;
+    cout << sSmallest;
+
+
 }
