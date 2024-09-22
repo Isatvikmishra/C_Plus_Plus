490be505ebefdb9b13f423e2107d9468f7d1997d
isatvikmishra <asksatvik@gmail.com>
2024-09-22 12:02:26 +0530

    Made functions

--- Array Questions/IMP_Second_Smallest_Second_Largest_Element.cpp
+++ Array Questions/IMP_Second_Smallest_Second_Largest_Element.cpp
@@ -1,6 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int secondLargest(vector<int> &arr, int n){
+    int largest = arr[0];
+    int sLargest = INT_MIN;
+    
+    for(int i = 1; i < n; i++){
+        if(arr[i] > largest){
+            sLargest = largest;
+            largest = arr[i];
+            
+        }
+        else if (arr[i] < largest && arr[i] > sLargest){
+            sLargest = arr[i];
+        }
+    }
+    return sLargest;
+ 
+}
+
+int secondSmallest(vector<int> &arr, int n){
+    int smallest = arr[n-1];
+    int sSmallest = INT_MAX;
+
+    for(int i=1; i<n; i++){
+        if (arr[i] < smallest){
+            sSmallest = smallest;
+            smallest = arr[i];
+        } 
+        else if (arr[i] != smallest && arr[i] < sSmallest){
+            sSmallest = arr[i];
+        }
+    }
+    return sSmallest;
+
+}
 
 int main(){
    
@@ -30,27 +64,9 @@ vector<int> arr = {4,3,4,5,2};
     */
 
 //!Optimal approach
- int largest = arr[0];
- int sLargest = INT_MIN;
- 
- int smallest = arr[n-1];
- int sSmallest = INT_MAX;
+    int sLargest = secondLargest(arr, n);
+    int sSmallest = secondSmallest(arr,n);
 
- for(int i = 1; i < n; i++){
-    if(arr[i] > largest){
-        largest = arr[i];
-        sLargest = arr[i-1];
-    }
-    else if (arr[i] < smallest){
-        sSmallest = smallest;
-        smallest = arr[i];
-    }
-    else if (arr[i] != smallest && arr[i] < sSmallest){
-        sSmallest = arr[i];
-    }
- }
     cout << sLargest << endl;
     cout << sSmallest;
-
-
 }
