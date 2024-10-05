52d3ce4231718edffae368eb9bfff920d40e8c85
isatvikmishra <asksatvik@gmail.com>
2024-10-06 00:33:46 +0530

    arr probs

--- Array Questions/Longest_Consecutive_Sequence.cpp
+++ Array Questions/Longest_Consecutive_Sequence.cpp
@@ -2,7 +2,59 @@
 using namespace std;
 
 
+int longestElement(vector<int> &arr, int n){
+   
+   /*
+    unordered_set<int> st;
+    for(int i =0; i<n; i++){
+        st.insert(arr[i]);
+    }
+*/
+    unordered_set<int> st(arr.begin(), arr.end());
+    
+    int longest = 1;
 
+    for(auto it : st){
+        if(st.find(it - 1) == st.end()){
+            int ctr = 1;
+            int x = it;
+
+            while(st.find(x + 1) != st.end()){
+                x = x+1;
+                ctr +=1;
+            }
+            longest = max(longest, ctr);
+        }
+    }
+    return longest;
+}
+
+
+//! TC: O(NlogN) + O(N), O(NLogN) is of sorting;
+/*
+int longestElement(vector<int> &arr, int n){
+    int longest = 1; 
+    int lastsmaller = INT_MIN;
+    int ctr = 0;
+    
+    sort(arr.begin(), arr.end());
+
+    for(int i=0; i<n; i++){
+        if(arr[i] - 1 == lastsmaller){
+            ctr +=1;
+            lastsmaller = arr[i];
+        }
+
+        else if(arr[i] != lastsmaller){
+            ctr = 1;
+            lastsmaller = arr[i];
+        }
+
+        longest = max(longest, ctr);    
+    }
+    return longest;
+}
+*/
 
 //! TC: O(n) × O(n) = O(n^2)
 /*
@@ -36,7 +88,7 @@ return longest;
 
 
 int main(){
-    vector<int> arr = {12,1,2,5,4,8,3};
+    vector<int> arr = {100, 200, 1, 1, 1, 1, 2, 2, 2, 2, 23, 3, 3, 101, 3, 4, 6,5};
     int n = arr.size();
 
     int ans = longestElement(arr, n);
